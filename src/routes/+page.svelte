fc6c6e8e013713f28b68c8ef85fa4904b355348a
SophiaStanley <148527526+SophiaStanley@users.noreply.github.com>
2026-04-28 14:18:33 +0100

    Changed images on game and title page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>The official website of the hit pop group!</h1>
-<img src = "/TitleTemp.png" alt = "Stellar Connection group shot"> <br>
+<img src = "/TitleScreen.png" alt = "Stellar Connection group shot"> <br>
 <hr>
 <div>
 <p>Stellar*Connection is a new pop group utilising their talents from across the entertainment industry to take the music world by storm. Consisting of Iris (Pop Singer and Model), Gwyn (Former Snow Boys member), Ace (An actor on 'Satellite Street'), Enid (A member of the dance troupe BURN IT BACK), and Elodie (The breakout star of Talent Academy Season 1), their unique perspectives provide "a fresh new take on pop music", as described by Marin Magazine.</p>
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,17 +1,18 @@
 <h1>Stellar*Stage!</h1>
 <p>Stellar*Stage is a rhythm game developed for Windows. Watch the Stellar*Connection members move to the rhythm as you play! Hit the notes in time with the music to increase your combo, and try to get the best score!</p>
-<img src = "/TitleScreen.png" alt = "Stellar Stage Title">
+<img src = "/StellarStageLogo.png" alt = "Stellar Stage Title">
 <br>
 <button> <a href = https://soupstantially.itch.io/stellarstage>Download Stellar*Stage!</a></button>
-<h1>Screenshots!</h1>
-<p>Coming soon!</p>
 
 <style>
     img {
     height: 50%;
     width: 50%;
     }
-
+    p {
+        padding-left: 20vw;
+        padding-right: 20vw;
+    }
 
     button {
         border: none;
